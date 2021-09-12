0c376b9f3a4552471f8f1ddf359f148115bc292a
auzmartist <auz.martin@gmail.com>
2021-09-12 16:17:32 -0700

    Release 1.1.0
    Features:
        - Add CamRef composition function
        - Clarify new components in Readme
        - Upgrade Hybrids to ^6.1 & required refactorings
        - Support 'hex' attribute on <cam-hsl> element
        - Support hex_rgb color conversion
        - Remove errant console.log in <cam-input type='range'>

--- src/cam-hsl.ts
+++ src/cam-hsl.ts
@@ -2,7 +2,7 @@ import { property, html, dispatch, Hybrids, define } from 'hybrids'
 import CamBox from './cam-box'
 import CamInput from './cam-input'
 import CamSwatch from './cam-swatch'
-import {hsl_rgb, rgb_hex} from './lib/color'
+import {hex_rgb, hsl_rgb, rgb_hex, rgb_hsl} from './lib/color'
 
 import styles from './cam-hsl.styl'
 
@@ -15,30 +15,39 @@ const CamHsl: Hybrids<any> = {
 	h: {
 		...ref(0),
 		observe: (host) => dispatch(host, 'change', {detail: {
-			h: host.h, s: host.s, l: host.l, hex: `#${host.hex}`
+			h: host.h, s: host.s, l: host.l, hex: `#${host._hex}`
 		}, bubbles: true, composed: true}),
 	},
 	s: {
 		...ref(100),
 		observe: (host) => dispatch(host, 'change', {detail: {
-			h: host.h, s: host.s, l: host.l, hex: `#${host.hex}`
+			h: host.h, s: host.s, l: host.l, hex: `#${host._hex}`
 		}, bubbles: true, composed: true}),
 	},
 	l: {
 		...ref(50),
 		observe: (host) => dispatch(host, 'change', {detail: {
-			h: host.h, s: host.s, l: host.l, hex: `#${host.hex}`
+			h: host.h, s: host.s, l: host.l, hex: `#${host._hex}`
 		}, bubbles: true, composed: true}),
 	},
 	a: {
 		...ref(1),
 		observe: (host) => dispatch(host, 'change', {detail: {
-			h: host.h, s: host.s, l: host.l, a: host.a, hex: `#${host.hex}`
+			h: host.h, s: host.s, l: host.l, a: host.a, hex: `#${host._hex}`
 		}, bubbles: true, composed: true}),
 	},
 	alpha: false,
-
-	hex: ({h, s, l}) => rgb_hex(hsl_rgb([h / 360, s / 100, l / 100])),
+	hex: {
+		...property(''),
+		observe: (host, hex) => {
+			if(!hex || hex.length !== 6) return
+			const [h, s, l] = rgb_hsl(hex_rgb(hex))
+			host.h = Math.round(h * 360)
+			host.s = Math.round(s * 100)
+			host.l = Math.round(l * 100)
+		}
+	},
+	_hex: ({h, s, l}) => rgb_hex(hsl_rgb([h / 360, s / 100, l / 100])),
 
 	render: ({h, s, l, a, alpha}) => html`
 		<div class="cam-hsl">
@@ -70,7 +79,7 @@ const CamHsl: Hybrids<any> = {
 				</cam-box>
 			</cam-swatch>
 		</div>
-	`.define({CamBox, CamInput, CamSwatch}).style(styles)
+	`.define(CamBox, CamInput, CamSwatch).style(styles)
 }
 
 define('cam-hsl', CamHsl)
